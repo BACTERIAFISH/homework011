5d398516b4d7833ae84d7dc4868e6e002bd465ac
Your Name <you@example.com>
2019-03-17 17:49:45 +0800

    2019 / 3 / 17

--- practice02/ViewController.swift
+++ practice02/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var setTimeLabel: UILabel!
+    @IBOutlet weak var nowTimeLabel: UILabel!
+    @IBOutlet weak var intervalTimeLabel: UILabel!
+    
+    var setTime = Date()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setTimeLabel.text = timeFormatter(setTime)
+        
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { (_) in
+            let nowString = self.timeFormatter(Date())
+            self.nowTimeLabel.text = nowString
+            
+            let interval = Int(self.setTime.timeIntervalSinceNow)
+            let second = interval % 60
+            let minute = interval / 60 % 60
+            let hour = interval / 3600 % 24
+            let day = interval / (3600 * 24)
+            self.intervalTimeLabel.text = "\(day)天 \(hour)小時 \(minute)分 \(second)秒"
+        })
+        
     }
 
+    func timeFormatter(_ time: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy年MM月dd日 HH:mm:ss"
+        return formatter.string(from: time)
+    }
+    
+    @IBAction func changeDatePicker(_ sender: UIDatePicker) {
+        setTime = sender.date
+        setTimeLabel.text = timeFormatter(sender.date)
+    }
+    
 
 }
 
